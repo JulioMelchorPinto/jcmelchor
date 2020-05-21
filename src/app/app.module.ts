a6fcace6338d352375dad0ef9e711d208ca27566
Julio Melchor Pinto <juliomelchorpinto@iMac-de-Julio.local>
2020-05-20 19:09:01 -0500

    fix responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { PrintPageComponent } from './pages/print-page/print-page.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { ProgressbarModule } from "ngx-bootstrap/progressbar";
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { SidebarComponent } from './pages/sidebar/sidebar.component';
     BrowserAnimationsModule,
     ButtonsModule.forRoot(),
     ProgressbarModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    ModalModule.forRoot(),
     FormsModule,
     FlexLayoutModule
   ],
